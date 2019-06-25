19b77a631f54b7a14a571ea717b2a7d20e95c657
Alfian Rehanusa Wibowo <wibowoalfian5229@gmail.com>
2019-06-25 13:33:31 +0700

    Create function register in model

--- application/controllers/UsersController.php
+++ application/controllers/UsersController.php
@@ -6,5 +6,10 @@ class UsersController extends CI_Controller {
 	public function __construct(){
         parent::__construct();
         $this->load->model('user');
+	}
+	
+	public function register(){
+		var_dump("You are in the routing register");
+		exit;
     }
 }
--- application/models/User.php
+++ application/models/User.php
@@ -2,5 +2,23 @@
 
 class User extends CI_Model
 {
+	//function for register user
+	public function save(){
 
+		//post value
+        $data   = [
+            'email'     => $this->input->post('email'),
+            'password'  => password_hash($this->input->post('password'), PASSWORD_DEFAULT)
+		];
+
+		//insert to database
+		$result = $this->db->insert('users',$data);
+        if($result){
+            return [
+                'id'        => $this->db->insert_id(),
+                'status'    => true,
+                'message'   => 'User successfully registered'
+            ];
+        }
+    }
 }
