a80ed12640bca0464cbccdbc57dbfcbb55b90be4
Alfian Rehanusa Wibowo <wibowoalfian5229@gmail.com>
2019-06-25 13:36:18 +0700

    Create dynamic json response

--- application/controllers/UsersController.php
+++ application/controllers/UsersController.php
@@ -8,6 +8,15 @@ class UsersController extends CI_Controller {
         $this->load->model('user');
 	}
 	
+	public function response($data){
+        $this->output
+            ->set_content_type('application/json')
+            ->set_status_header(200)
+            ->set_output(json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
+            ->_display();
+        exit;
+    }
+
 	public function register(){
 		$this->user->save();
     }
