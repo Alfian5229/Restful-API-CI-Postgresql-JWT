132759324632179e589a233acb46d3ac9b37aeac
Alfian Rehanusa Wibowo <wibowoalfian5229@gmail.com>
2019-06-25 13:25:50 +0700

    Inject a model in the controller

--- application/controllers/UsersController.php
+++ application/controllers/UsersController.php
@@ -3,4 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class UsersController extends CI_Controller {
 
+	public function __construct(){
+        parent::__construct();
+        $this->load->model('user');
+    }
 }
